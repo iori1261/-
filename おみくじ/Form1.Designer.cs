1ac9952d0a47d39b513fd7ce106c883f96e71db4
236137 <236137@KCSF0971>
2024-12-03 09:28:23 +0900

    プロジェクト ファイルを追加します。

---
+++ おみくじ/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace おみくじ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLottery = new System.Windows.Forms.Button();
+            this.lblFortune = new System.Windows.Forms.Label();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblD = new System.Windows.Forms.Label();
+            this.lblL = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblI = new System.Windows.Forms.Label();
+            this.lblFr = new System.Windows.Forms.Label();
+            this.lblDr = new System.Windows.Forms.Label();
+            this.lblLr = new System.Windows.Forms.Label();
+            this.lblSr = new System.Windows.Forms.Label();
+            this.lblBr = new System.Windows.Forms.Label();
+            this.lblIr = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLottery
+            // 
+            this.btnLottery.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.btnLottery.Location = new System.Drawing.Point(170, 384);
+            this.btnLottery.Name = "btnLottery";
+            this.btnLottery.Size = new System.Drawing.Size(309, 53);
+            this.btnLottery.TabIndex = 0;
+            this.btnLottery.Text = "無料で診断！";
+            this.btnLottery.UseVisualStyleBackColor = true;
+            this.btnLottery.Click += new System.EventHandler(this.btnLottery_Click);
+            // 
+            // lblFortune
+            // 
+            this.lblFortune.Font = new System.Drawing.Font("MS UI Gothic", 48F);
+            this.lblFortune.Location = new System.Drawing.Point(225, 6);
+            this.lblFortune.Name = "lblFortune";
+            this.lblFortune.Size = new System.Drawing.Size(201, 87);
+            this.lblFortune.TabIndex = 1;
+            this.lblFortune.Text = "運勢";
+            // 
+            // lblS
+            // 
+            this.lblS.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblS.Location = new System.Drawing.Point(48, 262);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(77, 23);
+            this.lblS.TabIndex = 4;
+            this.lblS.Text = "学問：";
+            // 
+            // lblD
+            // 
+            this.lblD.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblD.Location = new System.Drawing.Point(48, 184);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(77, 23);
+            this.lblD.TabIndex = 5;
+            this.lblD.Text = "願望：";
+            // 
+            // lblL
+            // 
+            this.lblL.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblL.Location = new System.Drawing.Point(48, 220);
+            this.lblL.Name = "lblL";
+            this.lblL.Size = new System.Drawing.Size(77, 23);
+            this.lblL.TabIndex = 6;
+            this.lblL.Text = "恋愛：";
+            // 
+            // lblB
+            // 
+            this.lblB.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblB.Location = new System.Drawing.Point(48, 301);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(77, 23);
+            this.lblB.TabIndex = 7;
+            this.lblB.Text = "商売：";
+            // 
+            // lblI
+            // 
+            this.lblI.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblI.Location = new System.Drawing.Point(48, 340);
+            this.lblI.Name = "lblI";
+            this.lblI.Size = new System.Drawing.Size(77, 23);
+            this.lblI.TabIndex = 8;
+            this.lblI.Text = "病気：";
+            // 
+            // lblFr
+            // 
+            this.lblFr.Location = new System.Drawing.Point(53, 93);
+            this.lblFr.Name = "lblFr";
+            this.lblFr.Size = new System.Drawing.Size(552, 77);
+            this.lblFr.TabIndex = 9;
+            // 
+            // lblDr
+            // 
+            this.lblDr.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblDr.Location = new System.Drawing.Point(131, 184);
+            this.lblDr.Name = "lblDr";
+            this.lblDr.Size = new System.Drawing.Size(474, 23);
+            this.lblDr.TabIndex = 10;
+            // 
+            // lblLr
+            // 
+            this.lblLr.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblLr.Location = new System.Drawing.Point(131, 220);
+            this.lblLr.Name = "lblLr";
+            this.lblLr.Size = new System.Drawing.Size(474, 23);
+            this.lblLr.TabIndex = 11;
+            // 
+            // lblSr
+            // 
+            this.lblSr.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblSr.Location = new System.Drawing.Point(131, 262);
+            this.lblSr.Name = "lblSr";
+            this.lblSr.Size = new System.Drawing.Size(474, 23);
+            this.lblSr.TabIndex = 12;
+            // 
+            // lblBr
+            // 
+            this.lblBr.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblBr.Location = new System.Drawing.Point(131, 301);
+            this.lblBr.Name = "lblBr";
+            this.lblBr.Size = new System.Drawing.Size(474, 23);
+            this.lblBr.TabIndex = 13;
+            // 
+            // lblIr
+            // 
+            this.lblIr.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblIr.Location = new System.Drawing.Point(131, 340);
+            this.lblIr.Name = "lblIr";
+            this.lblIr.Size = new System.Drawing.Size(474, 23);
+            this.lblIr.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 462);
+            this.Controls.Add(this.lblIr);
+            this.Controls.Add(this.lblBr);
+            this.Controls.Add(this.lblSr);
+            this.Controls.Add(this.lblLr);
+            this.Controls.Add(this.lblDr);
+            this.Controls.Add(this.lblFr);
+            this.Controls.Add(this.lblI);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblL);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.lblFortune);
+            this.Controls.Add(this.btnLottery);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLottery;
+        private System.Windows.Forms.Label lblFortune;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.Label lblL;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblI;
+        private System.Windows.Forms.Label lblFr;
+        private System.Windows.Forms.Label lblDr;
+        private System.Windows.Forms.Label lblLr;
+        private System.Windows.Forms.Label lblSr;
+        private System.Windows.Forms.Label lblBr;
+        private System.Windows.Forms.Label lblIr;
+    }
+}
+
---
+++ おみくじ/Form1.cs
@@ -0,0 +1,41 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace おみくじ
+{
+    public partial class Form1 : Form
+    {
+        //接続情報
+        String connection;
+        Random random = new Random();
+
+        public Form1()
+        {
+            String connection = "Server=localhost;Database=Major;UserID=root;Password=kcsf;Poolng=true;";
+            InitializeComponent();
+        }
+
+        private void btnLottery_Click(object sender, EventArgs e)
+        {
+            
+            int randomValue3 = random.Next(1, 8);
+            
+            MySqlConnection con = new MySqlConnection(connection);
+            MySqlCommand cmdFortune = new MySqlCommand("SELECT * FROM fortune;", con);
+            MySqlCommand cmdF = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+            MySqlCommand cmdD = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+            MySqlCommand cmdL = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+            MySqlCommand cmdS = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+            MySqlCommand cmdB = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+            MySqlCommand cmdI = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+        }
+    }
+}
