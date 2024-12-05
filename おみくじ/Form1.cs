dfe12ec0df483114df9a653f1ebcd768061261ec
iori1261 <kyhz.iori.maill@gmail.com>
2024-12-05 09:57:34 +0900

    Form1.cs

--- おみくじ/Form1.cs
+++ おみくじ/Form1.cs
@@ -1,41 +1,58 @@
-﻿using MySqlConnector;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+    using MySqlConnector;
+    using System.Windows.Forms;
+    using System;
 
-namespace おみくじ
-{
-    public partial class Form1 : Form
+    namespace おみくじ
     {
-        //接続情報
-        String connection;
-        Random random = new Random();
-
-        public Form1()
+        public partial class Form1 : Form
         {
-            String connection = "Server=localhost;Database=Major;UserID=root;Password=kcsf;Poolng=true;";
-            InitializeComponent();
-        }
+            // 接続情報
+            private string connectionString;
+            private Random random = new Random();
 
-        private void btnLottery_Click(object sender, EventArgs e)
-        {
-            
-            int randomValue3 = random.Next(1, 8);
-            
-            MySqlConnection con = new MySqlConnection(connection);
-            MySqlCommand cmdFortune = new MySqlCommand("SELECT * FROM fortune;", con);
-            MySqlCommand cmdF = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
-            MySqlCommand cmdD = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
-            MySqlCommand cmdL = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
-            MySqlCommand cmdS = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
-            MySqlCommand cmdB = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
-            MySqlCommand cmdI = new MySqlCommand("SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = 1 AND fd.category = 'A';", con);
+            public Form1()
+            {
+                connectionString = "Server=localhost;Database=Major;UserID=root;Password=kcsf;";
+                InitializeComponent();
+            }
+
+            private void btnLottery_Click(object sender, EventArgs e)
+            {
+                using (MySqlConnection con = new MySqlConnection(connectionString))
+                {
+                    con.Open();
+                    int randomValue = random.Next(1, 8);
+
+                    //各メッセージのSQL
+                    MySqlCommand cmdFortune = new MySqlCommand($"SELECT fortunerole FROM fortune WHERE id = {randomValue};", con);
+                    MySqlCommand cmdF = new MySqlCommand($"SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = {randomValue} AND fd.category = 'F';", con); // 全体運
+                    MySqlCommand cmdD = new MySqlCommand($"SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = {randomValue} AND fd.category = 'D';", con); // 願望
+                    MySqlCommand cmdL = new MySqlCommand($"SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = {randomValue} AND fd.category = 'L';", con); // 恋愛
+                    MySqlCommand cmdS = new MySqlCommand($"SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = {randomValue} AND fd.category = 'S';", con); // 学問
+                    MySqlCommand cmdB = new MySqlCommand($"SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = {randomValue} AND fd.category = 'B';", con); // 商売
+                    MySqlCommand cmdI = new MySqlCommand($"SELECT fd.message FROM Fortune f JOIN FortuneDetail fd ON f.id = fd.id WHERE f.id = {randomValue} AND fd.category = 'I';", con); // 病気
+
+                    //結果を表示
+                    lblFortune.Text = GetMessageFromCommand(cmdFortune);
+                    lblFr.Text = GetMessageFromCommand(cmdF);
+                    lblDr.Text = GetMessageFromCommand(cmdD);
+                    lblLr.Text = GetMessageFromCommand(cmdL);
+                    lblSr.Text = GetMessageFromCommand(cmdS);
+                    lblBr.Text = GetMessageFromCommand(cmdB);
+                    lblIr.Text = GetMessageFromCommand(cmdI);
+                }
+            }
+            //SQLからメッセージを取得
+            private string GetMessageFromCommand(MySqlCommand command)
+            {
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return reader.GetString(0);
+                    }
+                }
+                return "データがありません";
+            }
         }
     }
-}
